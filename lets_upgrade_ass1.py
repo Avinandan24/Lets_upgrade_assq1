ebb1898795a4de4f5d34579d9ac2d9e2d4149961
Avinandan Panda <105408004+Avinandan24@users.noreply.github.com>
2024-01-04 20:12:46 +0530

    Add files via upload

---
+++ lets_upgrade_ass1.py
@@ -0,0 +1,8 @@
+num = int(input("enter your number: "))
+print (num)
+if num > 0:
+    print ("Wow!! your number is positive")
+elif num == 0:
+    print ("Ohh.. it's zero")
+else:
+    print ("Oops!! your number is negative")   
